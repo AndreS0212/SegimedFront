c8db84e79bbfc8bbc52734e8af67fb30cd82ec9b
Andre <andre021297@gmail.com>
2024-12-13 02:30:00 -0500

    first commit

---
+++ app/dashboard/overview/page.tsx
@@ -0,0 +1,9 @@
+import OverViewPage from '../../../components/graphics/overview';
+
+export const metadata = {
+  title: 'Dashboard : Overview'
+};
+
+export default function page() {
+  return <OverViewPage />;
+}
